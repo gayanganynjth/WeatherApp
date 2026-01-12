8876c41c6c3d2462882e930c4c8ca89933dc7de0
Gayanga Nayanajith <gayangansilva@gamail.com>
2026-01-13 01:05:55 +0530

    Updated App.html

--- App.js
+++ App.js
@@ -12,6 +12,20 @@ function search() {
     httpRequest.onload = function() {
         let response = httpRequest.response;
         console.log(response);
+
+        document.getElementById("city-name").innerHTML = response.name;
+        document.getElementById("temp").innerHTML = response.main.temp;
+
+        if(response.weather[0].icon == "04n" || response.weather[0].icon == "04d") {
+            document.getElementById("weather-icon").src = "Images/broken-clouds.png";
+        }else if(response.weather[0].icon == "01n" || response.weather[0].icon == "01d") {
+            document.getElementById("weather-icon").src = "Images/sun.png";
+        }else if(response.weather[0].icon == "03n" || response.weather[0].icon == "03d") {
+            document.getElementById("weather-icon").src = "Images/scattered-clouds.png";
+        }
+
+        document.getElementById("wind").innerHTML = response.wind.speed + " Km/h";
+        document.getElementById("humidity").innerHTML = response.main.humidity + " %";
     }
 }
 
